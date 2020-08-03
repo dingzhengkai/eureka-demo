93cc15fca7b10cde571a15d283b72db4469a632a
dingzhengkai <xstack@163.com>
2020-08-03 20:40:27 +0800

    修改包名

---
+++ complete/eureka-service-consumer/src/main/java/com/example/service/consumer/SingleConsumerApplication.java
@@ -0,0 +1,61 @@
+package com.example.service.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients
+public class SingleConsumerApplication {
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingleConsumerApplication.class, args);
+    }
+}
+
+/**
+ * IHelloService
+ * 配置服务提供者：single-provider 是服务提供者的 application.name
+ */
+@FeignClient("single-provider")
+interface IHelloService {
+    @RequestMapping(value = "/hello")
+    String echoHello();
+}
+
+@RestController
+class SingleConsumerController {
+    private final RestTemplate restTemplate;
+    private final IHelloService helloService;
+
+    private static final String applicationName = "single-provider";
+
+    public SingleConsumerController(RestTemplate restTemplate, IHelloService helloService) {
+        this.restTemplate = restTemplate;
+        this.helloService = helloService;
+    }
+
+    @RequestMapping(value = "feignRequest")
+    public Object feignRequest() {
+        return helloService.echoHello();
+    }
+
+    @RequestMapping(value = {"/","/hello"})
+    public Object commonRequest() {
+        String url = "//" + applicationName + "/hello";
+        return restTemplate.getForObject(url, String.class);
+    }
+}
---
+++ complete/eureka-service-provider/src/main/java/com/example/service/provider/SingleProviderApplication.java
@@ -0,0 +1,42 @@
+package com.example.service.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SingleProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingleProviderApplication.class, args);
+    }
+}
+
+@RestController
+class ServiceProviderRestController {
+
+    private final DiscoveryClient discoveryClient;
+
+    public ServiceProviderRestController(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+    @RequestMapping(value = {"/", "/hello"})
+    public String echoHello() {
+        return "Hello";
+    }
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+}
